a6f5476c2f3ba2eb9944dc6dae87e7b79afe494b
ofeiipe <costadecarvalho.felipe@gmail.com>
2024-11-12 21:54:06 -0300

    commit

--- coleta.py
+++ coleta.py
@@ -18,13 +18,9 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
 #coloquei outro item para busca porque o carrefour só tinha 1 piano
-driver.get("https://www.carrefour.com.br/busca/tv?price=2916%3A49999")
+driver.get("https://www.carrefour.com.br/busca/tv")
 
-for i in range(15):
-    titulo = driver.find_elements('xpath', f'//*[@id="root"]/div/div[2]/div/div[2]/a[{i}]/div[2]/h2')
-    i+=1
-    lista_titulos.append(t.text for t in titulo)
-
+titulos = driver.find_elements('xpath', '//*[@id="root"]/div/div[2]/div/div[2]/a/div[2]/h2')
 precos = driver.find_elements('xpath', '//*[@id="root"]/div/div[2]/div/div[2]/a/div[2]/div/div[1]/span')
 links = driver.find_elements('xpath', '//*[@id="root"]/div/div[2]/div/div[2]/a')
 
@@ -33,6 +29,7 @@ for preco in precos:
     lista_precos.append(preco_format)
 
 lista_links = [l.get_attribute("href") for l in links]
+lista_titulos = [titulo.text for titulo in titulos]
 dataFrame['preco'] = lista_precos
 dataFrame['link'] = lista_links
 dataFrame['titulo'] = lista_titulos
@@ -43,6 +40,6 @@ idx_min = df['preco'].idxmin()
 
 menor_valor = df.loc[idx_min]
 
-print("Nome: ",menor_valor.titulo)
-print("Preço: ",menor_valor.preco)
-print("Link: ",menor_valor.link)
+print(f"Nome: {menor_valor.titulo}")
+print(f"Preço: R$:{menor_valor.preco}")
+print(f"Link: {menor_valor.link}")
